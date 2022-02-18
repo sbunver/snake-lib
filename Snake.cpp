5617d9cb1e10d2586e81b846d023a0fdf2485b28
Şafak Batuhan Ünver <sbunver@hotmail.com>
2022-02-19 01:39:01 +0300

    add point type and TODOs

--- Snake.cpp
+++ Snake.cpp
@@ -1,21 +1,40 @@
 #include <iostream>
 #include "Snake.hpp"
+
+SnakePoint moves[SNAKE_MOVE_COUNT] = 
+{
+    {-1, 0},
+    {0, 1},
+    {1, 0},
+    {0, -1}
+};
     
 Snake::Snake()
 {
     this->head = nullptr;
     this->tail = nullptr;
     
-    addSnakeNode(0,0);
-    addSnakeNode(1,0);
-    addSnakeNode(2,0); 
+    // Add first 3 body part of snake
+    SnakePoint p;
+    p.posX = 0;
+    p.posY = 0;
+    
+    addSnakeNodeToHead(p);
+    
+    p.posX = 1;
+    p.posY = 0;
+    addSnakeNodeToHead(p);
+    
+    p.posX = 2;
+    p.posY = 0;
+    addSnakeNodeToHead(p); 
 }
 
-void Snake::addSnakeNode(unsigned int x, unsigned int y)
+void Snake::addSnakeNodeToHead(SnakePoint& point)
 {
     SnakeNode* node = new SnakeNode;
-    node->posX = x;
-    node->posY = y;
+    node->position.posX = point.posX;
+    node->position.posY = point.posY;
 
     if(this->head == nullptr)
     {
@@ -24,11 +43,19 @@ void Snake::addSnakeNode(unsigned int x, unsigned int y)
     }
     else
     {
-        this->tail->next = node;
-        this->tail = this->tail->next;
+        node->next = this->head;
+        this->head = node;
     }
 
     return;
+}
+
+void Step()
+{
+    // TODO calculate next head cell according to move direction
+    // TODO if next head cell is valid
+    // TODO add new head cell to head of snake
+    // TODO shift all snake parT OF linked list
 }
 
 void Snake::printSnake()
@@ -36,7 +63,7 @@ void Snake::printSnake()
     SnakeNode* index = this->head;
     while(index != nullptr)
     {
-        std::cout<<index->posX<<" "<<index->posY<<std::endl;
+        std::cout<<index->position.posX<<" "<<index->position.posY<<std::endl;
         index = index->next;
     }
 }
--- Snake.hpp
+++ Snake.hpp
@@ -1,10 +1,24 @@
 #ifndef SNAKE_H
 #define SNAKE_H
 
+enum SNAKE_MOVE
+{
+    SNAKE_MOVE_UP,
+    SNAKE_MOVE_RIGHT,
+    SNAKE_MOVE_DOWN,
+    SNAKE_MOVE_LEFT,
+    SNAKE_MOVE_COUNT
+};
+
+struct SnakePoint
+{
+    int posX;
+    int posY;
+};
+
 struct SnakeNode
 {
-    unsigned int posX;
-    unsigned int posY;
+    SnakePoint position;
     SnakeNode* next;
 };
 
@@ -12,9 +26,10 @@ class Snake
 {
     public:
     Snake();
+    void Step();
     void printSnake();
     private:
-    void addSnakeNode(unsigned int x, unsigned int y);
+    void addSnakeNodeToHead(SnakePoint& point);
     SnakeNode* head;
     SnakeNode* tail;
 };
